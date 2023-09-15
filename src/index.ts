804e5536be687ade98e95833461c7c9638373727
vudinhduong <duong2lophot@gmail.com>
2023-09-15 14:57:19 +0700

    fix:vercel.json

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import { connectDB, createIndexAuthor, createIndexPost, createIndexTag, createIndexTopic } from '~/models/connectDB/connectDb'
 import dotenv from 'dotenv'
 import { routes } from '~/routes/index.routes'
-import { consfigENV } from './contants/env.config';
-import { handleError } from './middlewares/errorhandle';
+import { consfigENV } from './contants/env.config'
+import { handleError } from './middlewares/errorhandle'
 import cors from 'cors'
 dotenv.config()
 const app = express()
@@ -14,13 +14,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/', (req, res) => {
   return res.send('server on')
 })
+
 app.use(
   cors({
     "origin": "http://localhost:3000",
     "methods": "GET,PUT,POST,DELETE",
     "allowedHeaders": ['Content-Type', 'Authorization']
-  })
-)
+  }))
+
 connectDB().then(() => {
   createIndexTopic()
   createIndexTag()
